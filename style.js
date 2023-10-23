49528b3a70eb9613c562db08f4bb4690e63ed6e8
allegra-legume <130398142+allegra-legume@users.noreply.github.com>
2023-10-22 19:01:34 -0700

    Delete style.js

--- style.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $("#mobileMenu").click(function() {
-      if ($("#mobileSideBar").hasClass("is-active")) {
-          $("#mobileSideBar").removeClass("is-active");
-      } else {
-          $("#mobileSideBar").addClass("is-active");
-      }
-  });
-});
